9d04a2f982dbbea2ad310d9f099f6eecc916eca2
Shina <53410646+s7tya@users.noreply.github.com>
2022-08-09 02:37:08 +0900

    wip: update terraform

--- terraform/elastic_ip.tf
+++ terraform/elastic_ip.tf
@@ -1,7 +1,9 @@
 resource "aws_eip" "minecraft_eip" {
-	instance = aws_instance.minecraft_instance.id	
+  # instance = aws_instance.minecraft_instance.id
+  instance = aws_spot_instance_request.minecraft_instance.spot_instance_id
+  vpc      = true
 }
 
-output "server ip of minecraft" {
-	value = "${aws_eip.minecraft_eip.public_ip}"
+output "server_ip_of_minecraft" {
+  value = aws_eip.minecraft_eip.public_ip
 }
